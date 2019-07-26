56f5b465102990f448486201d585f846e6ec350b
yusuke031414 <49522987+yusuke031414@users.noreply.github.com>
2019-07-26 18:34:43 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,47 @@
+                var doughnutData = [
+                    {
+                      value: 11,
+                      color:"#9bcad0",
+                      highlight: "#a7d7de",
+                      label: "育児"
+                    },
+                    {
+                      value: 6,
+                      color: "#d5a87f",
+                      highlight: "#e4b68a",
+                      label: "睡眠"
+                    },
+                    {
+                      value: 4,
+                      color: "#edef9c",
+                      highlight: "#fcfea6",
+                      label: "FREETIME"
+                    },
+                   {
+                      value: 2,
+                      color: "#cca9ca",
+                      highlight: "#dcb7da",
+                      label: "炊事洗濯"
+                    },
+                   {
+                      value: 1,
+                      color: "#99FF99",
+                      highlight: "#CCFF99",
+                      label: "その他(移動、病院等)"
+                    }
+                  ];
+                  window.onload = function(){
+                    var ctx = document.getElementById("chart").getContext("2d");
+                    window.myDoughnut = new Chart(ctx).Doughnut(doughnutData, {
+                      responsive : true
+                    });
+                  }
+
+$(function(){
+
+  $('.logo').click(function(){
+
+    $('.logo').hide();
+
+  });
+});
